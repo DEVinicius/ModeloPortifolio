2ab37694618a5a945f4e2fe5055928d92db873e3
Vinicius Pereira de Oliveira <agavi2014@hotmail.com>
2020-02-15 10:11:15 -0300

    Adição da função ScrollAnimation

--- script.js
+++ script.js
@@ -2,4 +2,20 @@
 const query = document.querySelectorAll('[data-anime]');
 
 /*Nome da classe criada dinamicamente */
-const ScrollClass = "animate"
+const ScrollClass = 'animate';
+
+function scrollAnimation(){
+    var windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    query.forEach(function(element){
+        if((windowTop) > element.offsetTop){
+            element.classList.add(ScrollClass);
+        }
+        else{
+            element.classList.remove(ScrollClass);
+        }
+    });
+}
+
+window.addEventListener("scroll", function(){
+    scrollAnimation();
+});
